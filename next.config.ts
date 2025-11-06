897b24ceb37e4fdc069f4b26ee30eb84aae6ff28
Imam Rizky Pratama <140227265+imamrpratama@users.noreply.github.com>
2025-11-06 21:25:52 +0800

    Update : change image path

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // for GitHub Pages (static export)
-  basePath: "/portfolio", // base folder name in your GitHub Pages URL
-  assetPrefix: "/portfolio/", // ensures all static assets resolve correctly
+  /* config options here */
 };
 
 export default nextConfig;
